86bf81dde675beaa6ee41ed611dec37cf234b32d
Natalie Budeiko <bun@MacBook-Pro.local>
2019-04-21 13:39:54 +0300

    Homework 3.4

---
+++ src/homework3_4/Rubles.java
@@ -0,0 +1,25 @@
+package homework3_4;
+
+import java.util.Scanner;
+
+public class Rubles {
+    public static void main(String[] args) {
+
+        int roubles = getIntFromKeyboard();
+
+        int remainder = roubles % 10;
+        if (remainder == 1) {
+            System.out.println(roubles + " рубль");
+        } else if (remainder == 2 || remainder == 3 || remainder == 4) {
+            System.out.println(roubles + " рубля");
+        } else {
+            System.out.println(roubles + " рублей");
+        }
+
+    }
+
+    private static int getIntFromKeyboard() {
+        return Integer.parseInt(System.console().readLine());
+        //return new Scanner(System.in).nextInt();
+    }
+}

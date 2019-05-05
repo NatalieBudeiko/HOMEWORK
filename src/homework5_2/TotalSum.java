5d15f655fe5afb117ea59244dbe3e092d0753da4
Natalie Budeiko <nataliebudeiko@gmail.com>
2019-05-05 11:49:52 +0300

    homework5.2

--- src/homework5_2/TotalSum.java
+++ src/homework5_2/TotalSum.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class TotalSum {
     public static void main(String[] args) {
         System.out.println("Please enter your number");
+        long num = new Scanner(System.in).nextLong();
+        long sum = 0;
 
-        int num = new Scanner(System.in).nextInt();
+        while (num > 0) {
+            long lastNum = num % 10;
+            sum = sum + lastNum;
+            num = num / 10;
+        }
 
+        System.out.println("the sum is " + sum);//10
     }
 }
